4d7804c1d041595075ece0eb77c114f0b8c5185f
Abdul Moueed <69650688+Fs0ci3ty19@users.noreply.github.com>
2022-09-20 01:31:25 +0500

    Add files via upload

---
+++ Blog-Webpage/js/anime.js
@@ -0,0 +1,71 @@
+//Y-axis
+const observer = new  IntersectionObserver((entries)=>{
+
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+    
+    
+});
+
+
+const hidenElements =document.querySelectorAll('.hidden');
+hidenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
+//left x-aixs
+const observerX = new  IntersectionObserver((entries)=>{
+
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('Lxshow');
+        } else {
+            entry.target.classList.remove('Lxshow');
+        }
+    });
+    
+    
+});
+
+
+const hidenElementsX =document.querySelectorAll('.Lxhidden');
+hidenElementsX.forEach((el) => observerX.observe(el));
+
+
+
+
+
+
+//right x-axis
+const observerR = new  IntersectionObserver((entries)=>{
+
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('Rxshow');
+        } else {
+            entry.target.classList.remove('Rxshow');
+        }
+    });
+    
+    
+});
+
+    
+
+const hidenElementsR =document.querySelectorAll('.Rxhidden');
+hidenElementsR.forEach((el) => observerR.observe(el));
+
+
+
+
+
---
+++ Blog-Webpage/js/index.js
@@ -0,0 +1,89 @@
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    navText: ['<span class="fas fa-chevron-left fa-3x"></span>', '<span class="fas fa-chevron-right fa-3x"></span>'],
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: false
+        },
+        1000: {
+            items: 4,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+
+/*
+const button = document.querySelector('button');
+
+
+function greet() {
+    const name = prompt('What is your name!?');
+    alert(`Hello ${name} nice to meet you!`);
+}
+
+button.addEventListener('click',greet);*/
+
+
+
+
+
+
+
+
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 50) {
+      document.getElementById('navbar_top').classList.add('fixed-top');
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+    } else {
+      document.getElementById('navbar_top').classList.remove('fixed-top');
+      // remove padding top from body
+      document.body.style.paddingTop = '0';
+    }
+  });
+}); 
